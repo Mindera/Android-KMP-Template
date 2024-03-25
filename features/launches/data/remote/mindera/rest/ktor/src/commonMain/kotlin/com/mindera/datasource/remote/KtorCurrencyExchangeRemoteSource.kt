43c1195718305050572cbee9e979df37daaa8bfa
pawansoni <pawan.soni@mindera.com>
2024-03-25 12:47:44 +0530

    MKMPT-5: update mapper

--- features/launches/data/remote/mindera/rest/ktor/src/commonMain/kotlin/com/mindera/datasource/remote/KtorCurrencyExchangeRemoteSource.kt
+++ features/launches/data/remote/mindera/rest/ktor/src/commonMain/kotlin/com/mindera/datasource/remote/KtorCurrencyExchangeRemoteSource.kt
@@ -1,15 +1,12 @@
 package com.mindera.datasource.remote
 
 import com.mindera.datasource.remote.converters.toDomain
-import com.mindera.datasource.remote.converters.toRatesItemDomain
 import com.mindera.kmpexample.launches.domain.datasource.remote.CurrencyExchangeRemoteSource
 import com.mindera.rest.CurrencyExchangeResponseItem
-import com.mindera.rest.RatesItem
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import com.mindera.kmpexample.launches.domain.model.CurrencyExchangeResponseItem as DomainCurrencyExchangeResponseItem
-import com.mindera.kmpexample.launches.domain.model.RatesItem as DomainRates
 
 class KtorCurrencyExchangeRemoteSource constructor(
     private val baseUrl: String,
@@ -24,15 +21,8 @@ class KtorCurrencyExchangeRemoteSource constructor(
     private suspend fun getCurrencyExchange(table: String): List<DomainCurrencyExchangeResponseItem> =
         client.get("$baseUrl/$table")
             .body<List<CurrencyExchangeResponseItem>>().map {
-                val list = ArrayList<DomainRates>()
-                it.rates.forEach { rItem ->
-                    list.add(getCurrencyExchangeRates(rItem))
-                }
-                it.toDomain(list)
+                it.toDomain()
             }
 
-
-    override suspend fun getCurrencyExchangeRates(item: RatesItem): DomainRates =
-        item.toRatesItemDomain()
 
 }
--- features/launches/data/remote/mindera/rest/ktor/src/commonMain/kotlin/com/mindera/datasource/remote/converters/Api.kt
+++ features/launches/data/remote/mindera/rest/ktor/src/commonMain/kotlin/com/mindera/datasource/remote/converters/Api.kt
@@ -1,17 +1,28 @@
 package com.mindera.datasource.remote.converters
 
-import com.mindera.kmpexample.launches.domain.model.RatesItem
 import com.mindera.rest.CurrencyExchangeResponseItem
 import com.mindera.uuid
-import com.mindera.kmpexample.launches.domain.model.CurrencyExchangeResponseItem as DomainCurrencyExchangeResponseItem
 
-internal fun CurrencyExchangeResponseItem.toDomain(ratesItems: List<RatesItem>) = DomainCurrencyExchangeResponseItem(
-    rates = ratesItems,
-)
+//internal fun CurrencyExchangeResponseItem.toDomain(ratesItems: List<RatesItem>) = DomainCurrencyExchangeResponseItem(
+//    rates = ratesItems,
+//)
+//
+//internal fun com.mindera.rest.RatesItem.toRatesItemDomain() = RatesItem(
+//    id = uuid(), // this is just a sample we usually don't need it as is
+//    code = code,
+//    currencyRate = currencyRate,
+//    currency = currency,
+//)
 
-internal fun com.mindera.rest.RatesItem.toRatesItemDomain() = RatesItem(
-    id = uuid(), // this is just a sample we usually don't need it as is
-    code = code,
-    currencyRate = currencyRate,
-    currency = currency,
-)
+internal fun CurrencyExchangeResponseItem.toDomain() =
+    com.mindera.kmpexample.launches.domain.model.CurrencyExchangeResponseItem(
+        rates = rates.map { it.toDomain() },
+    )
+
+internal fun com.mindera.rest.RatesItem.toDomain() =
+    com.mindera.kmpexample.launches.domain.model.RatesItem(
+        id = uuid(), // this is just a sample we usually don't need it as is
+        code = code,
+        currencyRate = currencyRate,
+        currency = currency,
+    )
