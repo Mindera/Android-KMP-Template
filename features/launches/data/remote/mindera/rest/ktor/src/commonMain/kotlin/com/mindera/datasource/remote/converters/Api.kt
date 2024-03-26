a652d053d627da5e204ea9e02ce406b249d52b5e
pawansoni <pawan.soni@mindera.com>
2024-03-26 15:17:39 +0530

    MKMPT-5: remove comment

--- features/launches/data/remote/mindera/rest/ktor/src/commonMain/kotlin/com/mindera/datasource/remote/converters/Api.kt
+++ features/launches/data/remote/mindera/rest/ktor/src/commonMain/kotlin/com/mindera/datasource/remote/converters/Api.kt
@@ -3,17 +3,6 @@ package com.mindera.datasource.remote.converters
 import com.mindera.rest.CurrencyExchangeResponseItem
 import com.mindera.uuid
 
-//internal fun CurrencyExchangeResponseItem.toDomain(ratesItems: List<RatesItem>) = DomainCurrencyExchangeResponseItem(
-//    rates = ratesItems,
-//)
-//
-//internal fun com.mindera.rest.RatesItem.toRatesItemDomain() = RatesItem(
-//    id = uuid(), // this is just a sample we usually don't need it as is
-//    code = code,
-//    currencyRate = currencyRate,
-//    currency = currency,
-//)
-
 internal fun CurrencyExchangeResponseItem.toDomain() =
     com.mindera.kmpexample.launches.domain.model.CurrencyExchangeResponseItem(
         rates = rates.map { it.toDomain() },
