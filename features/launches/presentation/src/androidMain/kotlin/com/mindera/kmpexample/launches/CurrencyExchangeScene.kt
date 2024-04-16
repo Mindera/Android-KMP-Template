5f6a1f6967ae7c03008052513d29fb1ec0a51354
pawansoni <pawan.soni@mindera.com>
2024-04-16 14:41:26 +0530

    MKMPT-3: Code update

--- features/launches/presentation/src/androidMain/kotlin/com/mindera/kmpexample/launches/CurrencyExchangeScene.kt
+++ features/launches/presentation/src/androidMain/kotlin/com/mindera/kmpexample/launches/CurrencyExchangeScene.kt
@@ -1,39 +1,21 @@
 package com.mindera.kmpexample.launches
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults.SecondaryIndicator
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.mindera.kmpexample.composables.CurrencyExchangeScreen
-import com.mindera.kmpexample.features.launches.presentation.R
 import com.mindera.kmpexample.launches.viewmodel.CurrencyExchangeViewModel
 import com.mindera.precompose.navigation.BackHandler
 import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
@@ -57,23 +39,20 @@ class KoinHelper : KoinComponent {
 fun CurrencyExchangeScene(onBack: (() -> Unit)) {
     BackHandler(onBack = onBack)
 
-    var checked by remember { mutableStateOf(true) }
+    var tabClick by remember { mutableStateOf(0) }
     val viewModel = remember { KoinHelper().viewModel }
     val modelProducer = remember { CartesianChartModelProducer.build() }
 
     val lastTenState = viewModel.lastTenCurrencyStateFlow.collectAsState()
     val currentState = viewModel.currentCurrencyStateFlow.collectAsState()
-    var tabIndex by remember { mutableIntStateOf(0) }
-    val tabs = listOf(stringResource(R.string.graph_view), stringResource(R.string.list_view))
 
+    val list = ArrayList<Double>()
+    val listCurrency = ArrayList<String>()
     val emptyList = emptyList<String>()
     var bottomAxisValueFormatter: AxisValueFormatter<AxisPosition.Horizontal.Bottom> =
         AxisValueFormatter { x, _, _ ->
             emptyList[x.toInt() % 0]
         }
-
-    val list = ArrayList<Double>()
-    val listCurrency = ArrayList<String>()
 
 
     currentState.value.launches.forEach {
@@ -90,7 +69,7 @@ fun CurrencyExchangeScene(onBack: (() -> Unit)) {
                         }
                     }
 
-                    bottomAxisValueFormatter = AxisValueFormatter{ x, _, _ ->
+                    bottomAxisValueFormatter = AxisValueFormatter { x, _, _ ->
                         listCurrency[x.toInt() % listCurrency.size]
                     }
                 }
@@ -100,64 +79,14 @@ fun CurrencyExchangeScene(onBack: (() -> Unit)) {
 
 
     Column(modifier = Modifier.fillMaxSize()) {
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(10.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = painterResource(R.drawable.mindera),
-                contentDescription = "heading",
-                modifier = Modifier.height(50.dp)
-            )
-            Text(
-                text = stringResource(R.string.mindera),
-                fontSize = 25.sp,
-            )
-        }
-        Box(
-            modifier = Modifier.background(color = colorResource(R.color.yellow)).height(10.dp)
-                .fillMaxWidth()
-        )
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(bottom = 5.dp),
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            TabRow(
-                selectedTabIndex = tabIndex,
-                containerColor = colorResource(R.color.lightYellow),
-                contentColor = Color.Black,
-                indicator = { tabPositions ->
-                    if (tabIndex < tabPositions.size) {
-                        SecondaryIndicator(
-                            modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
-                            color = Color.Black
-                        )
-                    }
-                }
-            ) {
-                tabs.forEachIndexed { index, title ->
-                    Tab(text = { Text(title) },
-                        selected = tabIndex == index,
-                        onClick = { tabIndex = index },
-                        selectedContentColor = Color.Black,
-                        unselectedContentColor = Color.Gray,
-                    )
-                }
-            }
-            when (tabIndex) {
-                0 -> checked = false
-                1 -> checked = true
-            }
+        HomeHeaderScene()
+        HomeTabRowScene {
+            tabClick = it
         }
         Box(modifier = Modifier.weight(weight = 1f).fillMaxWidth()) {
-            if (checked) {
-                CurrencyExchangeScreen(
-                    currencies = lastTenState.value.launches
-                )
-            } else {
-                CartesianChartHost(modifier = Modifier.fillMaxHeight().padding(bottom = 20.dp),
+            if (tabClick == 0) {
+                CartesianChartHost(
+                    modifier = Modifier.fillMaxHeight().padding(bottom = 20.dp),
                     chart = rememberCartesianChart(
                         rememberLineCartesianLayer(),
                         startAxis = rememberStartAxis(),
@@ -167,6 +96,10 @@ fun CurrencyExchangeScene(onBack: (() -> Unit)) {
                     ),
                     modelProducer = modelProducer,
                     zoomState = rememberVicoZoomState(zoomEnabled = false),
+                )
+            } else {
+                CurrencyExchangeScreen(
+                    currencies = lastTenState.value.launches
                 )
             }
         }
