b5c9d6444b2080fccea5a4e4d2d4c1729f0dc95b
pawansoni <pawan.soni@mindera.com>
2024-04-18 16:02:49 +0530

    MKMPT-11: local update

--- features/launches/presentation/src/androidMain/kotlin/com/mindera/kmpexample/launches/language/LocaleHelper.kt
+++ features/launches/presentation/src/androidMain/kotlin/com/mindera/kmpexample/launches/language/LocaleHelper.kt
@@ -1,8 +1,6 @@
 package com.mindera.kmpexample.launches.language
 
-import android.annotation.TargetApi
 import android.content.Context
-import android.os.Build
 import com.mindera.spName
 import java.util.Locale
 
@@ -11,11 +9,8 @@ object LocaleHelper {
 
     const val spLanguage = "language"
 
-    fun setLocale(context: Context, language: String): Context? {
+    fun setLocale(context: Context, language: String): Context {
         setLanguageSharedPreference(context, language)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            return updateResources(context, language)
-        }
         return updateResourcesLegacy(context, language)
     }
 
@@ -36,23 +31,12 @@ object LocaleHelper {
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.N)
-    private fun updateResources(context: Context, language: String): Context? {
-        val locale = Locale(language)
-        Locale.setDefault(locale)
-        val configuration = context.resources.configuration
-        configuration.setLocale(locale)
-        configuration.setLayoutDirection(locale)
-        return context.createConfigurationContext(configuration)
-    }
-
     private fun updateResourcesLegacy(context: Context, language: String): Context {
         val locale = Locale(language)
         Locale.setDefault(locale)
         val resources = context.resources
         val configuration = resources.configuration
-        configuration.locale = locale
-        resources.updateConfiguration(configuration, resources.displayMetrics)
-        return context
+        configuration.setLocale(locale)
+        return context.createConfigurationContext(configuration)
     }
 }
