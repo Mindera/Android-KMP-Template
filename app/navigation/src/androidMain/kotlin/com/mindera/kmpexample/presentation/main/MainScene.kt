2cdb3b9df4936e0e364f29ffa5674c7fd811d95e
pawansoni <pawan.soni@mindera.com>
2024-05-03 16:03:05 +0530

    MKMPT-12: Open Source screen

---
+++ app/navigation/src/androidMain/kotlin/com/mindera/kmpexample/presentation/main/MainScene.kt
@@ -0,0 +1,40 @@
+package com.mindera.kmpexample.presentation.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import com.mindera.kmpexample.launches.HomeScreenScene
+import com.mindera.kmpexample.launches.opensource.OpensourceScene
+import com.mindera.kmpexample.presentation.main.Destination.CurrencyExchange
+import com.mindera.kmpexample.presentation.main.Destination.OpenSource
+import com.mindera.precompose.navigation.NavHost
+import com.mindera.precompose.navigation.scene
+import com.mindera.precompose.navigation.transition.NoTransition
+import moe.tlaster.precompose.navigation.rememberNavigator
+
+sealed class Destination(
+    override val route: String,
+) : com.mindera.precompose.navigation.Destination {
+    data object CurrencyExchange : Destination("currency-exchange")
+    data object OpenSource : Destination("opensource")
+}
+
+@Composable
+fun MainScene(
+    /* Callback */
+    onBack: () -> Unit,
+) {
+    val navigator = rememberNavigator()
+
+    NavHost(
+        navTransition = remember { NoTransition },
+        navigator = navigator,
+        initialRoute = CurrencyExchange,
+    ) {
+        scene(route = CurrencyExchange) {
+            HomeScreenScene(onBack = onBack, navigator = navigator)
+        }
+        scene(route = OpenSource) {
+            OpensourceScene(navigator = navigator)
+        }
+    }
+}
