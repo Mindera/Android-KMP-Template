54e72e03b4ea54b4d1bde3ef15dba0435c8124e7
Pawan Soni <pawan.soni@mindera.com>
2024-06-07 16:29:18 +0530

    MKMPT-20: CurrencyExchangeRemoteSource format

--- features/currencyexchange/domain/src/commonMain/kotlin/com/mindera/kmpexample/currencyexchange/domain/datasource/remote/CurrencyExchangeRemoteSource.kt
+++ features/currencyexchange/domain/src/commonMain/kotlin/com/mindera/kmpexample/currencyexchange/domain/datasource/remote/CurrencyExchangeRemoteSource.kt
@@ -4,15 +4,8 @@ import com.mindera.kmpexample.currencyexchange.domain.model.CurrencyExchangeResp
 import com.mindera.rest.CurrencyExchangeResponseItem
 
 interface CurrencyExchangeRemoteSource {
-
     suspend fun getCurrencyExchange(endpoint: String): List<DomainCurrencyExchangeResponseItem>
-
     fun insertCurrency(currency: CurrencyExchangeResponseItem)
-
-
     suspend fun getAllCurrencies(): List<DomainCurrencyExchangeResponseItem>
-
     suspend fun removeAllCurrencies()
-
-
 }
